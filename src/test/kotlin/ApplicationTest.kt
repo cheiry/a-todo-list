6076a8622370a46f99defcab566b318f22fafd81
cheiry <cheiry4@gmail.com>
2025-04-29 23:53:40 +0200

    feat: edit status, improve tests

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,10 +1,11 @@
 package com.cheiry
 
-import io.ktor.client.HttpClient
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.json
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -13,12 +14,14 @@ class ApplicationTest {
 
     @Test
     fun `get tasks returns ok`() = testApplication {
-        application {
-            module()
-        }
-        client.get("/tasks").apply {
+        val client = setup()
+        val response = client.get("/tasks")
+        response.apply {
             assertEquals(HttpStatusCode.OK, status)
         }
+        val tasks: Array<Task> = response.body()
+        assertEquals("Get a life", tasks[0].name)
+        assertEquals(Status.NEW, tasks[0].status)
     }
 
     @Test
@@ -28,6 +31,8 @@ class ApplicationTest {
             contentType(ContentType.Application.Json)
             setBody(Task(4, "Test"))
         }.apply { assertEquals(HttpStatusCode.Created, status) }
+        val tasks: Array<Task> = client.get("/tasks").body()
+        assertEquals("Test", tasks[3].name)
     }
 
     @Test
@@ -37,6 +42,31 @@ class ApplicationTest {
             contentType(ContentType.Application.Json)
             setBody(Task(1, "Test"))
         } .apply { assertEquals(HttpStatusCode.BadRequest, status) }
+    }
+
+    @Test
+    fun `post new task with status`() = testApplication {
+        val client = setup()
+        client.post("/tasks") {
+            contentType(ContentType.Application.Json)
+            setBody(Task(5, "Test", Status.TODO))
+        } .apply { assertEquals(HttpStatusCode.Created, status) }
+        val tasks: Array<Task> = client.get("/tasks").body()
+        assertEquals(Status.TODO, tasks.last().status)
+    }
+
+    @Test
+    fun `put update task with status`() = testApplication {
+        val client = setup()
+        client.post("/tasks") {
+            contentType(ContentType.Application.Json)
+            setBody(Task(6, "Test"))
+        }
+        client.put("/tasks/6/status/TODO") {
+            contentType(ContentType.Application.Json)
+        } .apply { assertEquals(HttpStatusCode.NoContent, status) } // TODO : maybe change the return code ?
+        val tasks: Array<Task> = client.get("/tasks").body()
+        assertEquals(Status.TODO, tasks.last().status)
     }
 
     private fun ApplicationTestBuilder.setup(): HttpClient {
